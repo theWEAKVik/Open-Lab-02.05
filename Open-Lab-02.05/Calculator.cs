6a8a29ada20796e98b9c1e831d70276fe94ca37a
Erik Zemčík <zemcik.erik@gmail.com>
2019-10-24 17:34:40 +0200

    Created test

---
+++ Open-Lab-02.05/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_02._05
+{
+    public class Calculator
+    {
+        public bool DivisibleByFive(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Open-Lab-02.05/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Open_Lab_02._05
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private Calculator calculator;
+
+        [OneTimeSetUp]
+        public void Init() => calculator = new Calculator();
+
+        [Test]
+        public void DivisibleByFiveTest([Range(0, 299, 3)] int number) =>
+            Assert.That(calculator.DivisibleByFive(number), Is.EqualTo(number % 5 == 0));
+
+    }
+}
